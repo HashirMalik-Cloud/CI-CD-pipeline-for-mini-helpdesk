0365303f1bc4d75e05d194af1f4bfe50f856ceab
Hashir Malik <hashir.gggggggg@gmail.com>
2025-09-09 19:01:59 +0500

    Fix providers.tf and commit lockfile for consistent providers

--- infra/providers.tf
+++ infra/providers.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 5.100.0"
+      version = "~> 5.0"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "= 2.7.1"
+      version = "~> 2.4"
     }
     random = {
       source  = "hashicorp/random"
-      version = "= 3.7.2"
+      version = "~> 3.6"
     }
   }
 
